006255d1078e4d5dc068e29db3612f833bb25113
sawko <sawko1987.s@gmail.com>
2024-11-03 07:28:27 +0300

    дата

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name="Александр"
+print("Name:",name)
+age=37
+print("Age:",age)
+age=25
+print("New age:",age)
+is_student=age>=25
+print("is_student:", is_student)
---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name="Александр"
+print("Name:",name)
+age=37
+print("Age:",age)
+age=25
+print("New age:",age)
+is_student=True
+print("is_student:", is_student)
