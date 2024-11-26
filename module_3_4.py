a982401a7b305e1d87adfcccae4d72f31081e7b7
sawko1987 <sawko1987.s@gmail.com>
2024-11-26 10:16:32 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,18 @@
+
+def single_root_words (root_word,*other_words):
+    same_words = []
+    for i in other_words:
+        if ("".join(i).lower()) not in (root_word).lower() and  root_word.lower() not in ("".join(i).lower()):
+            continue
+        else:
+            same_words.append(i)
+    return same_words
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+
+
+
+
+
